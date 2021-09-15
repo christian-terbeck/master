a79711382ae8d71f47cac5ecc5d4189b1bc0d650
Christian Terbeck <mail@christian-terbeck.de>
2021-09-15 18:09:58 +0200

    Adjust test code

--- resources/views/sensor-test.blade.php
+++ resources/views/sensor-test.blade.php
@@ -6,15 +6,26 @@
 // https://github.com/intel/generic-sensor-demos
 // https://w3c.github.io/orientation-sensor/#relativeorientationsensor-interface
 
-import { RelativeOrientationSensor } from '../../js/sensors.js';
+// import { RelativeOrientationSensor } from '../../js/sensors.js';
+import { Gyroscope } from '../../js/sensors.js';
 
-const options = { frequency: 60, referenceFrame: 'device' };
+const sensor = new Gyroscope();
+
+sensor.start();
+
+sensor.addEventListener('reading', () => {
+    document.getElementById('output').innerHTML = 'X: ' + sensor.x + '<br>Y: ' + sensor.y + '<br>Z: ' + sensor.z;
+});
+
+console.log(sensor);
+
+/*const options = { frequency: 60, referenceFrame: 'device' };
 const sensor = new RelativeOrientationSensor(options);
 
 sensor.addEventListener('reading', () => {
     // model is a Three.js object instantiated elsewhere.
     // model.quaternion.fromArray(sensor.quaternion).inverse();
-    document.getElementById('output').innerHTML = sensor.quaternion;
+    // document.getElementById('output').innerHTML = sensor.quaternion;
 });
 
 sensor.addEventListener('error', error => {
@@ -25,9 +36,9 @@ sensor.addEventListener('error', error => {
 
 sensor.start();
 
-console.log(sensor.quaternion);
+console.log(sensor.quaternion);*/
 // console.log(Object.getOwnPropertyNames(sensor));
-document.getElementById('output').innerHTML = sensor.quaternion;
+//document.getElementById('output').innerHTML = sensor.quaternion;
 
 // window.ondeviceorientation = function(event) { console.log(event); };
 // window.addEventListener('deviceorientation', function(event) { console.log(event); });
